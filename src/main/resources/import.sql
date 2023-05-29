b2f1205bcdc6c042edae0e07544ebe0fa91d3df1
Gismii <gisme.guimaraes@gmail.com>
2023-05-28 21:36:50 -0300

    Cria conexão com o postgres

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -4,3 +4,21 @@
 -- insert into myentity (id, field) values(2, 'field-2');
 -- insert into myentity (id, field) values(3, 'field-3');
 -- alter sequence myentity_seq restart with 4;
+
+-- Script SQL para inserção de dados na tabela "customer"
+
+INSERT INTO customer (id, name, lastName, age, email) VALUES (1, 'João', 'Silva', 25, 'joao@example.com');
+INSERT INTO customer (id, name, lastName, age, email) VALUES (2, 'Maria', 'Souza', 30, 'maria@example.com');
+INSERT INTO customer (id, name, lastName, age, email) VALUES (3, 'Carlos', 'Ferreira', 28, 'carlos@example.com');
+INSERT INTO customer (id, name, lastName, age, email) VALUES (4, 'Ana', 'Pereira', 33, 'ana@example.com');
+INSERT INTO customer (id, name, lastName, age, email) VALUES (5, 'Pedro', 'Santos', 27, 'pedro@example.com');
+INSERT INTO customer (id, name, lastName, age, email) VALUES (6, 'Sandra', 'Almeida', 31, 'sandra@example.com');
+INSERT INTO customer (id, name, lastName, age, email) VALUES (7, 'Fernando', 'Oliveira', 29, 'fernando@example.com');
+INSERT INTO customer (id, name, lastName, age, email) VALUES (8, 'Mariana', 'Rodrigues', 35, 'mariana@example.com');
+INSERT INTO customer (id, name, lastName, age, email) VALUES (9, 'Ricardo', 'Lima', 26, 'ricardo@example.com');
+INSERT INTO customer (id, name, lastName, age, email) VALUES (10, 'Camila', 'Costa', 32, 'camila@example.com');
+INSERT INTO customer (id, name, lastName, age, email) VALUES (11, 'Gustavo', 'Sousa', 28, 'gustavo@example.com');
+INSERT INTO customer (id, name, lastName, age, email) VALUES (12, 'Isabela', 'Gomes', 34, 'isabela@example.com');
+INSERT INTO customer (id, name, lastName, age, email) VALUES (13, 'Antônio', 'Martins', 27, 'antonio@example.com');
+INSERT INTO customer (id, name, lastName, age, email) VALUES (14, 'Patrícia', 'Silveira', 33, 'patricia@example.com');
+INSERT INTO customer (id, name, lastName, age, email) VALUES (15, 'Felipe', 'Ribeiro', 30, 'felipe@example.com');
